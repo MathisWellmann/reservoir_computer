a84dc0d8589bf9efcfcfd475251527c00a6b0446
MathisWellmann <wellmannmathis@gmail.com>
2022-03-12 16:59:21 +0100

    put the reservoir into actual struct

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 #[macro_use]
 extern crate log;
 
-//mod reservoir;
 mod errors;
 mod load_sample_data;
+mod reservoir;
 
 use std::time::Instant;
 
-use nalgebra::{Const, DMatrix, Dim, Dynamic, Matrix, SymmetricEigen, VecStorage};
-use nanorand::{Rng, WyRand};
 use plotters::prelude::*;
+
+use crate::reservoir::ReservoirComputer;
 
 type Series = Vec<(f32, f32)>;
 
@@ -27,115 +27,13 @@ fn main() {
         series.iter().skip(1).zip(series.iter()).map(|(a, b)| (a / b).ln()).collect();
     info!("got {} datapoints", series.len());
 
-    const N: usize = 160; // reservoir size of hidden neurons
-    const M: usize = 1; // input and output dimension
     const TRAINING_WINDOW: usize = 10_000;
-
-    // For each node in the reservoir, select k nodesn the network without
-    // replacement and use those as inputs to the current node.
-    let fixed_in_degree_k = 5;
-    let input_sparsity = 0.3;
-    let input_scaling = 0.5;
-    let input_bias = 0.0;
-    // The spectral radius determines how fast the influence of an input
-    // dies out in a reservoir with time, and how stable the reservoir activations
-    // are. The spectral radius should be greater in tasks requiring longer
-    // memory of the input.
-    let spectral_radius = 0.9;
-    // The leaking rate a can be regarded as the speed of the reservoir update
-    // dynamics discretized in time. This can be adapted online to deal with
-    // time wrapping of the signals. Set the leaking rate to match the speed of
-    // the dynamics of input / target This sort of acts as a EMA smoothing
-    // filter, so other MAs could be used such as ALMA
-    let alpha = 0.005;
-    let train_frac = 0.7;
-    let split_idx = (series.len() as f64 * train_frac) as usize;
 
     let t0 = Instant::now();
 
-    //let mut rng = WyRand::new_seed(0);
-    let mut rng = WyRand::new();
-    let mut weights: Vec<Vec<f32>> = vec![vec![0.0; N]; N];
-    for j in 0..weights.len() {
-        for _ in 0..fixed_in_degree_k {
-            // Choose random input node
-            let i = rng.generate_range(0..N);
-            weights[i][j] = rng.generate::<f32>() * 2.0 - 1.0;
-        }
-    }
-    let mut reservoir: DMatrix<f32> = DMatrix::from_vec_generic(
-        Dim::from_usize(N),
-        Dim::from_usize(N),
-        weights.iter().cloned().flatten().collect(),
-    );
-    debug!("reservoir: {}", reservoir);
-    let eigen = SymmetricEigen::new(reservoir.clone());
-    let spec_rad = eigen.eigenvalues.abs().max();
-    reservoir *= (1.0 / spec_rad) * spectral_radius;
-
-    let input_matrix: Matrix<f32, Dynamic, Const<M>, VecStorage<f32, Dynamic, Const<M>>> =
-        Matrix::from_fn_generic(Dim::from_usize(N), Dim::from_usize(M), |_, _| {
-            if rng.generate::<f32>() < input_sparsity {
-                rng.generate::<f32>() * input_scaling
-            } else {
-                0.0
-            }
-        });
-    let mut readout_matrix: Matrix<f32, Const<M>, Dynamic, VecStorage<f32, Const<M>, Dynamic>> =
-        Matrix::from_fn_generic(Dim::from_usize(M), Dim::from_usize(N), |_, _| {
-            rng.generate::<f32>() * 2.0 - 1.0
-        });
-    let mut state: Matrix<f32, Dynamic, Const<1>, VecStorage<f32, Dynamic, Const<1>>> =
-        Matrix::from_fn_generic(Dim::from_usize(N), Dim::from_usize(1), |_, _| {
-            rng.generate::<f32>() * 2.0 - 1.0
-        });
-    debug!(
-        "input_matrix: {}\nreservoir: {}\nreadout_matrix: {}\nstate: {}",
-        input_matrix, reservoir, readout_matrix, state
-    );
-
-    let mut rmse: f32 = 0.0;
-    let mut step_wise_design: DMatrix<f32> =
-        DMatrix::from_fn_generic(Dim::from_usize(N), Dim::from_usize(TRAINING_WINDOW), |_, _| 0.0);
-    let mut step_wise_predictions: Matrix<
-        f32,
-        Const<M>,
-        Dynamic,
-        VecStorage<f32, Const<M>, Dynamic>,
-    > = Matrix::from_fn_generic(Dim::from_usize(M), Dim::from_usize(TRAINING_WINDOW), |_, _| 0.0);
-    let step_wise_target: Matrix<f32, Const<M>, Dynamic, VecStorage<f32, Const<M>, Dynamic>> =
-        Matrix::from_vec_generic(
-            Dim::from_usize(M),
-            Dim::from_usize(TRAINING_WINDOW),
-            rets.iter().take(TRAINING_WINDOW).cloned().collect(),
-        );
-    for (j, val) in rets.iter().enumerate().take(TRAINING_WINDOW) {
-        let predicted_out = &readout_matrix * &state;
-        step_wise_predictions.set_column(j, &predicted_out);
-
-        step_wise_design.set_column(j, &state);
-
-        let a = (1.0 - alpha) * &state;
-        let unit_vec: Matrix<f32, Dynamic, Const<1>, VecStorage<f32, Dynamic, Const<1>>> =
-            Matrix::from_element_generic(Dim::from_usize(N), Dim::from_usize(1), 1.0);
-        let mut b = &reservoir * &state + &input_matrix * *val + input_bias * unit_vec;
-        b.iter_mut().for_each(|v| *v = v.tanh());
-        state = a + alpha * b;
-
-        rmse += (predicted_out.get(0).unwrap() - *val).powi(2);
-    }
-    info!("rmse: {}", rmse);
-
-    // compute optimal readout matrix
-    let design_t = step_wise_design.transpose();
-    // Use regularizaion whenever there is a danger of overfitting or feedback
-    // instability
-    let regularization_coeff: f32 = 0.0;
-    let identity_m: DMatrix<f32> =
-        DMatrix::from_diagonal_element_generic(Dim::from_usize(N), Dim::from_usize(N), 1.0);
-    let b = step_wise_design * &design_t + regularization_coeff * identity_m;
-    let b = b.transpose();
-    readout_matrix = step_wise_target * &design_t * b;
+    let leaking_rate = 0.01;
+    let mut rc = ReservoirComputer::new(20, 5, 0.3, 0.0, 0.0, 0.95, leaking_rate, Some(1));
+    rc.train(&rets.iter().take(TRAINING_WINDOW).cloned().collect::<Vec<f32>>());
 
     let mut targets: Series = Vec::with_capacity(1_000_000);
     let mut predictions: Series = Vec::with_capacity(1_000_000);
@@ -147,11 +45,12 @@ fn main() {
 
     let mut train_predictions: Series = Vec::with_capacity(TRAINING_WINDOW);
 
+    let mut state = rc.state();
     for (i, val) in rets.iter().enumerate().skip(1).take(TRAINING_WINDOW * 2) {
         curr_val *= val.exp();
         targets.push((i as f32, curr_val));
 
-        let predicted_out = &readout_matrix * &state;
+        let predicted_out = rc.readout_matrix() * &state;
         let pred = predicted_out.get(0).unwrap();
         curr_pred *= pred.exp();
 
@@ -168,10 +67,10 @@ fn main() {
             *val
         };
 
-        let a = (1.0 - alpha) * &state;
-        let mut b = &reservoir * &state + &input_matrix * val;
+        let a = (1.0 - leaking_rate) * &state;
+        let mut b = rc.reservoir() * &state + rc.input_matrix() * val;
         b.iter_mut().for_each(|v| *v = v.tanh());
-        state = a + alpha * b;
+        state = a + leaking_rate * b;
     }
 
     info!("t_diff: {}ms", t0.elapsed().as_millis());
--- src/reservoir.rs
+++ src/reservoir.rs
@@ -1,46 +1,177 @@
-use nalgebra::{Const, Dim, OMatrix, SymmetricEigen};
+use nalgebra::{Const, DMatrix, Dim, Dynamic, Matrix, SymmetricEigen, VecStorage};
 use nanorand::{Rng, WyRand};
 
 /// The Reseoir Computer
 /// N is the number of reservoir nodes
 /// M is the input and output dimension
 #[derive(Debug)]
-pub(crate) struct Reservoir<const N: usize, const M: usize> {
-    reservoir: OMatrix<f32, Const<N>, Const<N>>,
+pub(crate) struct ReservoirComputer {
+    reservoir_size: usize,
+    leaking_rate: f32,
+    input_bias: f32,
+    input_matrix: Matrix<f32, Dynamic, Const<1>, VecStorage<f32, Dynamic, Const<1>>>,
+    reservoir: DMatrix<f32>,
+    readout_matrix: Matrix<f32, Const<1>, Dynamic, VecStorage<f32, Const<1>, Dynamic>>,
+    state: Matrix<f32, Dynamic, Const<1>, VecStorage<f32, Dynamic, Const<1>>>,
 }
 
-impl<const N: usize, const M :usize> Reservoir<N, M> {
+impl ReservoirComputer {
     /// Create a new reservoir, with random initiallization
     /// # Arguments
-    /// sparsity: the connection probability within the reservoir
-    /// spectral_radius:
+    /// reservoir_size: number of nodes in the reservoir
+    /// fixed_in_degree_k: number of inputs per node
+    /// input_sparsity: the connection probability within the reservoir
+    /// input_scaling: multiplies the input weights
+    /// input_bias: adds a bias to inputs
+    /// leaking_rate:
+    ///     The leaking rate a can be regarded as the speed of the reservoir
+    /// update     dynamics discretized in time. This can be adapted online
+    /// to deal with     time wrapping of the signals. Set the leaking rate
+    /// to match the speed of     the dynamics of input / target This sort
+    /// of acts as a EMA smoothing     filter, so other MAs could be used
+    /// such as ALMA spectral_radius:
+    ///     The spectral radius determines how fast the influence of an input
+    ///     dies out in a reservoir with time, and how stable the reservoir
+    /// activations     are. The spectral radius should be greater in tasks
+    /// requiring longer     memory of the input.
+    /// seed: optional RNG seed
     pub(crate) fn new(
-        sparsity: f32,
+        reservoir_size: usize,
+        fixed_in_degree_k: usize,
+        input_sparsity: f32,
+        input_scaling: f32,
+        input_bias: f32,
         spectral_radius: f32,
         leaking_rate: f32,
+        seed: Option<u64>,
     ) -> Self {
+        let mut rng = match seed {
+            Some(seed) => WyRand::new_seed(seed),
+            None => WyRand::new(),
+        };
+        let mut weights: Vec<Vec<f32>> = vec![vec![0.0; reservoir_size]; reservoir_size];
+        for j in 0..weights.len() {
+            for _ in 0..fixed_in_degree_k {
+                // Choose random input node
+                let i = rng.generate_range(0..reservoir_size);
+                weights[i][j] = rng.generate::<f32>() * 2.0 - 1.0;
+            }
+        }
+        let mut reservoir: DMatrix<f32> = DMatrix::from_vec_generic(
+            Dim::from_usize(reservoir_size),
+            Dim::from_usize(reservoir_size),
+            weights.iter().cloned().flatten().collect(),
+        );
+        debug!("reservoir: {}", reservoir);
+
+        let eigen = SymmetricEigen::new(reservoir.clone());
+        let spec_rad = eigen.eigenvalues.abs().max();
+        reservoir *= (1.0 / spec_rad) * spectral_radius;
+
+        let input_matrix: Matrix<f32, Dynamic, Const<1>, VecStorage<f32, Dynamic, Const<1>>> =
+            Matrix::from_fn_generic(Dim::from_usize(reservoir_size), Dim::from_usize(1), |_, _| {
+                if rng.generate::<f32>() < input_sparsity {
+                    rng.generate::<f32>() * input_scaling
+                } else {
+                    0.0
+                }
+            });
+        let mut readout_matrix: Matrix<f32, Const<1>, Dynamic, VecStorage<f32, Const<1>, Dynamic>> =
+            Matrix::from_fn_generic(Dim::from_usize(1), Dim::from_usize(reservoir_size), |_, _| {
+                rng.generate::<f32>() * 2.0 - 1.0
+            });
+        let mut state: Matrix<f32, Dynamic, Const<1>, VecStorage<f32, Dynamic, Const<1>>> =
+            Matrix::from_fn_generic(Dim::from_usize(reservoir_size), Dim::from_usize(1), |_, _| {
+                rng.generate::<f32>() * 2.0 - 1.0
+            });
+        debug!(
+            "input_matrix: {}\nreservoir: {}\nreadout_matrix: {}\nstate: {}",
+            input_matrix, reservoir, readout_matrix, state
+        );
 
         Self {
             reservoir,
+            input_matrix,
+            readout_matrix,
+            state,
+            reservoir_size,
+            leaking_rate,
+            input_bias,
         }
     }
 
-    /*
-    pub(crate) fn forward(&self, input: SVector<f32, M>) -> SVector<f32, M> {
-        todo!()
+    pub(crate) fn train(&mut self, values: &[f32]) {
+        let mut step_wise_design: DMatrix<f32> = DMatrix::from_fn_generic(
+            Dim::from_usize(self.reservoir_size),
+            Dim::from_usize(values.len()),
+            |_, _| 0.0,
+        );
+        let mut step_wise_predictions: Matrix<
+            f32,
+            Const<1>,
+            Dynamic,
+            VecStorage<f32, Const<1>, Dynamic>,
+        > = Matrix::from_fn_generic(Dim::from_usize(1), Dim::from_usize(values.len()), |_, _| 0.0);
+        let step_wise_target: Matrix<f32, Const<1>, Dynamic, VecStorage<f32, Const<1>, Dynamic>> =
+            Matrix::from_vec_generic(
+                Dim::from_usize(1),
+                Dim::from_usize(values.len()),
+                values.to_vec(),
+            );
+        for (j, val) in values.iter().enumerate() {
+            let predicted_out = &self.readout_matrix * &self.state;
+            step_wise_predictions.set_column(j, &predicted_out);
+
+            step_wise_design.set_column(j, &self.state);
+
+            let a = (1.0 - self.leaking_rate) * &self.state;
+            let unit_vec: Matrix<f32, Dynamic, Const<1>, VecStorage<f32, Dynamic, Const<1>>> =
+                Matrix::from_element_generic(
+                    Dim::from_usize(self.reservoir_size),
+                    Dim::from_usize(1),
+                    1.0,
+                );
+            let mut b = &self.reservoir * &self.state
+                + &self.input_matrix * *val
+                + self.input_bias * unit_vec;
+            b.iter_mut().for_each(|v| *v = v.tanh());
+            self.state = a + self.leaking_rate * b;
+        }
+
+        // compute optimal readout matrix
+        let design_t = step_wise_design.transpose();
+        // Use regularizaion whenever there is a danger of overfitting or feedback
+        // instability
+        let regularization_coeff: f32 = 0.0;
+        let identity_m: DMatrix<f32> = DMatrix::from_diagonal_element_generic(
+            Dim::from_usize(self.reservoir_size),
+            Dim::from_usize(self.reservoir_size),
+            1.0,
+        );
+        let b = step_wise_design * &design_t + regularization_coeff * identity_m;
+        let b = b.transpose();
+        self.readout_matrix = step_wise_target * &design_t * b;
     }
-    */
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    pub(crate) fn readout_matrix(
+        &self,
+    ) -> &Matrix<f32, Const<1>, Dynamic, VecStorage<f32, Const<1>, Dynamic>> {
+        &self.readout_matrix
+    }
 
-    #[test]
-    fn reservoir() {
-        if let Err(_) = pretty_env_logger::try_init() {}
+    pub(crate) fn reservoir(&self) -> &DMatrix<f32> {
+        &self.reservoir
+    }
 
-        let res = Reservoir::<10, 1>::new(0.1, 0.9, 0.1);
-        info!("res: {:?}", res);
+    pub(crate) fn input_matrix(
+        &self,
+    ) -> &Matrix<f32, Dynamic, Const<1>, VecStorage<f32, Dynamic, Const<1>>> {
+        &self.input_matrix
+    }
+
+    pub(crate) fn state(
+        &self,
+    ) -> Matrix<f32, Dynamic, Const<1>, VecStorage<f32, Dynamic, Const<1>>> {
+        self.state.clone()
     }
 }
