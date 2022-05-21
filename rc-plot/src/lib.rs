5c03b8c8d7bf1c895902b7dc2b75e9ed65de65c9
MathisWellmann <wellmannmathis@gmail.com>
2022-05-21 14:49:29 +0200

    rc-plot: made plot_gather into its own file. removed trade data

--- rc-plot/src/lib.rs
+++ rc-plot/src/lib.rs
@@ -4,10 +4,12 @@ extern crate log;
 mod gif_render;
 mod gif_render_optimizer;
 pub mod plot;
+mod plot_gather;
 
 pub use gif_render::GifRender;
 pub use gif_render_optimizer::GifRenderOptimizer;
 pub use plot::plot;
+pub use plot_gather::PlotGather;
 
 pub type Series = Vec<(f64, f64)>;
 
