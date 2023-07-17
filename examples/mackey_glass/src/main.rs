79812a989d2a74a9c664021431858eb6e81a3a3a
MathisWellmann <wellmannmathis@gmail.com>
2023-07-17 09:11:50 +0200

    Move `mackey_glass` series generation into `time_series_generator`

--- examples/mackey_glass/src/main.rs
+++ examples/mackey_glass/src/main.rs
@@ -1,14 +1,13 @@
 #[macro_use]
 extern crate log;
 
-use std::{collections::VecDeque, sync::Arc, time::Instant};
+use std::{sync::Arc, time::Instant};
 
 use classic_rcs::{ESNConstructor, EUSNConstructor, Params, RC};
 use common::{environments::EnvMackeyGlass, Activation, ReservoirComputer};
 use dialoguer::{theme::ColorfulTheme, Select};
 use lin_reg::TikhonovRegularization;
 use nalgebra::{DMatrix, Dim, Matrix};
-use nanorand::{Rng, WyRand};
 use next_generation_rcs::{NGRCConstructor, NextGenerationRC, Params as NGRCParams};
 use rc_plot::{plot, PlotGather};
 
@@ -18,7 +17,7 @@ const SEED: Option<u64> = Some(0);
 
 pub(crate) fn main() {
     let total_len = TRAIN_LEN + TEST_LEN;
-    let values = mackey_glass_series(total_len, 30, SEED);
+    let values = time_series_generator::mackey_glass_series(total_len, 30, SEED);
 
     let values: DMatrix<f64> =
         Matrix::from_vec_generic(Dim::from_usize(values.len()), Dim::from_usize(1), values);
@@ -470,42 +469,4 @@ pub(crate) fn main() {
 
         _ => panic!("invalid reservoir computer selection"),
     };
-}
-
-/// Mackey glass series generation
-/// # Parameters:
-/// sample_len: the number of datapoints to generate
-/// tau: roughly correlates to the chaotic and complex behaviour of the sequence
-/// seed: An optional seed for the random number generator
-pub(crate) fn mackey_glass_series(sample_len: usize, tau: usize, seed: Option<u64>) -> Vec<f64> {
-    let delta_t = 10;
-    let mut timeseries = 1.2;
-    let history_len = tau * delta_t;
-
-    let mut rng = match seed {
-        Some(seed) => WyRand::new_seed(seed),
-        None => WyRand::new(),
-    };
-    let mut history = VecDeque::with_capacity(history_len);
-    for _i in 0..history_len {
-        let val = 1.2 + 0.2 * (rng.generate::<f64>() - 0.5);
-        history.push_back(val);
-    }
-
-    let mut inp = vec![0.0; sample_len];
-
-    for item in inp.iter_mut().take(sample_len) {
-        for _ in 0..delta_t {
-            let x_tau = history.pop_front().unwrap();
-            history.push_back(timeseries);
-            let last_hist = history[history.len() - 1];
-            timeseries = last_hist
-                + (0.2 * x_tau / (1.0 + x_tau.powi(10)) - 0.1 * last_hist) / delta_t as f64;
-        }
-        *item = timeseries;
-    }
-    // apply tanh nonlinearity
-    inp.iter_mut().for_each(|v| *v = v.tanh());
-
-    inp
 }
